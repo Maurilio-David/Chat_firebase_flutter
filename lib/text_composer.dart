2cbbd7cf7090bb6a9fcc82e2ec82b532ff0263f0
maurilioconceicao <maurilio.conceicao@branef.com.br>
2022-01-07 23:42:10 -0300

    enviando mensagem e salvando no firebase

--- lib/text_composer.dart
+++ lib/text_composer.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 
 class TextComposer extends StatefulWidget {
-  const TextComposer({ Key? key }) : super(key: key);
+  TextComposer(this.sendMessage);
 
+  Function(String) sendMessage;
+  
   @override
   _TextComposerState createState() => _TextComposerState();
 }
 
 class _TextComposerState extends State<TextComposer> {
+  final TextEditingController _textController = TextEditingController();
   bool _isComposing = false;
-
+  _reset(){
+    _textController.clear();       
+    setState(() {
+      _isComposing = false;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -18,14 +26,23 @@ class _TextComposerState extends State<TextComposer> {
         IconButton(onPressed: (){
           
         }, icon: Icon(Icons.photo_camera)),
-        Expanded(child: TextField(decoration: InputDecoration.collapsed(hintText: 'Enviar uma mensagem'), onChanged: (text){
+        Expanded(child: TextField(decoration: 
+        InputDecoration.collapsed(hintText: 'Enviar uma mensagem'), 
+        onChanged: (text){
           setState(() {
             _isComposing = text.isNotEmpty;
           });
-        }, onSubmitted: (text){
-
+        
+        },
+        controller: _textController,
+         onSubmitted: (text){
+          widget.sendMessage(text);
+          _reset();
         },)),
-        IconButton(icon: Icon(Icons.send), onPressed: _isComposing? (){}: null)
+        IconButton(icon: Icon(Icons.send), onPressed: _isComposing? (){
+          widget.sendMessage(_textController.text);
+          _reset();
+        }: null)
       ],),
     );
   }
